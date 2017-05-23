aa8bab660e7ce2633a0e68b88b701932d05babdf
lwio <443474713@qq.com>
2017-05-23 14:39:52 +0800

    roughly finished card and add callback of tab

--- lwui-sass/tab.js
+++ lwui-sass/tab.js
@@ -10,7 +10,8 @@ Tabs();
 'use strict';
 
 function Tabs() {
-  var bindAll = function() {
+  var self = this;
+  function bindAll() {
     var menuElements = document.querySelectorAll('[data-tab]');
     for(var i = 0; i < menuElements.length ; i++) {
       menuElements[i].addEventListener('click', change, false);
@@ -29,13 +30,32 @@ function Tabs() {
   }
 
   var change = function(e) {
+    var fromTab = document.querySelector('.active[data-tab]').getAttribute("data-tab");
     clear();
     e.target.classList.add('active');
     var dataTab = e.currentTarget.getAttribute('data-tab');
     //add active class to target
     document.querySelectorAll('[data-tab=' + dataTab +']')[0].classList.add('active');
     document.querySelectorAll('[data-tab=' + dataTab +']')[1].classList.add('active');
+    emit('change', fromTab, dataTab);
   }
-
+  function emit(event) {
+    //if eventCollection not exist
+    if(!self.eventCollection) {
+      return self;
+    }
+    for(var cb of self.eventCollection[event]) {
+      cb.call(self, ...Array.prototype.slice.call(arguments, 1));
+    }
+  }
   bindAll();
+  return this;
+}
+Tabs.prototype.on = function(event, callback) {
+  // 使用现有的collection，如果不存在，则创建
+  this.eventCollection = this.eventCollection || {};
+  this.eventCollection[event] = this.eventCollection[event] || [];
+
+  // 讲给定的监听函数push进入指定事件的collection
+  this.eventCollection[event].push(callback);
 }
